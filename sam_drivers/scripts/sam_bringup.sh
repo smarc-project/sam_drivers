c5610d91ddc1d1df7c6e17e6069815ea77eed853
Nils Bore <nbore@kth.se>
2019-02-06 16:43:28 +0100

    Everything that we need

--- sam_drivers/scripts/sam_bringup.sh
+++ sam_drivers/scripts/sam_bringup.sh
@@ -7,18 +7,22 @@ tmux new-window -t $SESSION:0 -n 'roscore'
 tmux new-window -t $SESSION:1 -n 'sam_core'
 tmux new-window -t $SESSION:2 -n 'sam_dr'
 tmux new-window -t $SESSION:3 -n 'sam_controllers'
+tmux new-window -t $SESSION:4 -n 'sam_monitor'
 
 tmux select-window -t $SESSION:0
 tmux send-keys "roscore" C-m
 
 tmux select-window -t $SESSION:1
-tmux send-keys "roslaunch sam_drivers sam_core.launch"
+tmux send-keys "mon launch sam_drivers sam_core.launch"
 
 tmux select-window -t $SESSION:2
-tmux send-keys "roslaunch sam_dead_reckoning sam_stim_dr.launch"
+tmux send-keys "mon launch sam_dead_reckoning sam_stim_dr.launch"
 
 tmux select-window -t $SESSION:3
-tmux send-keys "roslaunch sam_drivers sam_controllers.launch"
+tmux send-keys "mon launch sam_drivers sam_controllers.launch"
+
+tmux select-window -t $SESSION:4
+tmux send-keys "mon launch sam_drivers sam_monitor.launch"
 
 # Set default window
 tmux select-window -t $SESSION:0
