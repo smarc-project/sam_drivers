46429d781ad91b280f5d97c287357e0253be686f
Nils Bore <nilsbore@gmail.com>
2019-04-23 17:20:27 +0200

    Forgot to include the extra things

--- sam_uavcan_bridge/src/uavcan_to_ros_bridge.cpp
+++ sam_uavcan_bridge/src/uavcan_to_ros_bridge.cpp
@@ -13,9 +13,7 @@
 #include <uavcan_ros_bridge/uav_to_ros/pressure.h>
 #include <uavcan_ros_bridge/uav_to_ros/sensor_pressure.h>
 
-/*
-#include <uavcan_ros_bridge/uav_to_ros/actuator_status.h>
-*/
+#include <sam_uavcan_bridge/uav_to_ros/actuator_status.h>
 
 extern uavcan::ICanDriver& getCanDriver(const std::string&);
 extern uavcan::ISystemClock& getSystemClock();
@@ -77,11 +75,8 @@ int main(int argc, char** argv)
     uav_to_ros::ConversionServer<smarc_uavcan_messages::SensorPressure, sensor_msgs::FluidPressure> sensor_pressure_server1(uav_node, pn, "sensor_pressure1", 1);
     uav_to_ros::ConversionServer<smarc_uavcan_messages::SensorPressure, sensor_msgs::FluidPressure> sensor_pressure_server2(uav_node, pn, "sensor_pressure2", 2);
     uav_to_ros::ConversionServer<smarc_uavcan_messages::SensorPressure, sensor_msgs::FluidPressure> motor_oil_pressure_server(uav_node, pn, "motor_oil_pressure", 20);
-
-    /*
     uav_to_ros::ConversionServer<uavcan::equipment::actuator::Status, sam_msgs::PercentStamped> vbs_feedback_server(uav_node, pn, "vbs_feedback", 13);
     uav_to_ros::ConversionServer<uavcan::equipment::actuator::Status, sam_msgs::PercentStamped> lcg_feedback_server(uav_node, pn, "lcg_feedback", 14);
-    */
 
     /*
      * Running the node.
