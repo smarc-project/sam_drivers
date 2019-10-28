e8405aee2b60e59e2a65052563f30efae04f78a7
Nils Bore <nbore@kth.se>
2019-10-28 14:06:14 +0100

    Preparing launch gui

--- sam_drivers/scripts/sam_bringup.sh
+++ sam_drivers/scripts/sam_bringup.sh
@@ -4,28 +4,32 @@ SESSION=sam_bringup
 tmux -2 new-session -d -s $SESSION
 
 tmux new-window -t $SESSION:0 -n 'roscore'
-tmux new-window -t $SESSION:1 -n 'sam_core'
-tmux new-window -t $SESSION:2 -n 'sam_dr'
-tmux new-window -t $SESSION:3 -n 'sam_controllers'
-tmux new-window -t $SESSION:4 -n 'sam_bt'
-tmux new-window -t $SESSION:5 -n 'sam_monitor'
+tmux new-window -t $SESSION:1 -n 'sam_gui'
+tmux new-window -t $SESSION:2 -n 'sam_core'
+tmux new-window -t $SESSION:3 -n 'sam_dr'
+tmux new-window -t $SESSION:4 -n 'sam_controllers'
+tmux new-window -t $SESSION:5 -n 'sam_bt'
+tmux new-window -t $SESSION:6 -n 'sam_monitor'
 
 tmux select-window -t $SESSION:0
 tmux send-keys "roscore" C-m
 
 tmux select-window -t $SESSION:1
-tmux send-keys "mon launch sam_drivers sam_core.launch"
+tmux send-keys "rosrun flexxros sam_controls.py" C-m
 
 tmux select-window -t $SESSION:2
-tmux send-keys "mon launch sam_dead_reckoning sam_stim_dr.launch"
+tmux send-keys "mon launch sam_drivers sam_core.launch --name=$(tmux display-message -p '#I_#W') --no-start" C-m
 
 tmux select-window -t $SESSION:3
-tmux send-keys "mon launch sam_drivers sam_controllers.launch"
+tmux send-keys "mon launch sam_dead_reckoning sam_stim_dr.launch --name=$(tmux display-message -p '#I_#W') --no-start" C-m
 
 tmux select-window -t $SESSION:4
-tmux send-keys "mon launch sam_march sam_march.launch"
+tmux send-keys "mon launch sam_drivers sam_controllers.launch --name=$(tmux display-message -p '#I_#W') --no-start" C-m
 
 tmux select-window -t $SESSION:5
+tmux send-keys "mon launch sam_march sam_march.launch --name=$(tmux display-message -p '#I_#W') --no-start" C-m
+
+tmux select-window -t $SESSION:6
 tmux send-keys "roslaunch sam_drivers sam_monitor.launch"
 
 # Set default window
