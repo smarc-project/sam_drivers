ad7730ccbe4c834f424e069131eb1cfdd905415d
xyp8023 <xyp8023@gmail.com>
2020-01-22 08:57:57 +0100

    ESC feeback combined into rpm_fb

--- sam_drivers/scripts/esc_fb_parser.py
+++ sam_drivers/scripts/esc_fb_parser.py
@@ -8,18 +8,19 @@ import message_filters
 class ESCParser(object):
 
     def esc_cb(self, esc_fb_msg0, esc_fb_msg1):
+        #print "Synch ESC msgs"
 	synch_fb = ThrusterRPMs()
-        snych_fb.thruster_1_rpm = esc_fb_msg0.esc_index
+        synch_fb.thruster_1_rpm = esc_fb_msg0.esc_index
         synch_fb.thruster_2_rpm = esc_fb_msg1.esc_index
-        synch_fb.header.stamp = rospy.get_time()
-	self.esc_pub.publish(latest_fb)
+        synch_fb.header.stamp = rospy.Time.now()
+	self.esc_pub.publish(synch_fb)
 
 
     def __init__(self):
-        self.esc_subs0 = message_filters.Subscriber("/sam/core/esc_status0_fb", ESCStatus)
-        self.esc_subs1 = message_filters.Subscriber("/sam/core/esc_status1_fb", ESCStatus)
-        self.esc_pub = rospy.Publisher("/sam/core/rpm_fb", ThrusterRPMs, queue_size=10)
-        self.ts = message_filters.TimeSynchronizer([self.esc_subs0, self.esc_subs1], 10)
+        self.esc_subs0 = message_filters.Subscriber("esc_status0_fb", ESCStatus)
+        self.esc_subs1 = message_filters.Subscriber("esc_status1_fb", ESCStatus)
+        self.esc_pub = rospy.Publisher("rpm_fb", ThrusterRPMs, queue_size=10)
+        self.ts = message_filters.ApproximateTimeSynchronizer([self.esc_subs0, self.esc_subs1], 10, slop=1.0, allow_headerless=False)
         self.ts.registerCallback(self.esc_cb)
 
         rospy.spin()
