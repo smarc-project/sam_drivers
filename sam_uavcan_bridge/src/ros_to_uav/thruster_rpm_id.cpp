d088438c9ec226ae5d1a90ebd3e31e1bec5040ec
Carl Ljung <carlju@kth.se>
2021-11-15 20:59:05 +0100

    implement new naming standard for thrusters

---
+++ sam_uavcan_bridge/src/ros_to_uav/thruster_rpm_id.cpp
@@ -0,0 +1,14 @@
+#include <sam_uavcan_bridge/ros_to_uav/thruster_rpm_id.h>
+
+namespace ros_to_uav {
+
+template <>
+bool convert(const smarc_msgs::ThrusterRPM& ros_msg, smarc_uavcan_messages::ThrusterRpmID& uav_msg, unsigned char uid)
+{
+    uav_msg.thruster.id = uid;
+    uav_msg.thruster.rpm = ros_msg.rpm;
+
+    return true;
+}
+
+}
