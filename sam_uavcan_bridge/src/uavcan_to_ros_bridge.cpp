f9ee0e9db591f18b94961dc459a304eafada34aa
Carl Ljung <carlju@kth.se>
2022-05-11 15:29:34 +0200

    add battery_info

--- sam_uavcan_bridge/src/uavcan_to_ros_bridge.cpp
+++ sam_uavcan_bridge/src/uavcan_to_ros_bridge.cpp
@@ -20,6 +20,7 @@
 #include <sam_uavcan_bridge/uav_to_ros/servo_feedback_double.h>
 #include <sam_uavcan_bridge/uav_to_ros/temperature.h>
 #include <sam_uavcan_bridge/uav_to_ros/battery_state_basic.h>
+#include <sam_uavcan_bridge/uav_to_ros/battery_info.h>
 // #include <sam_uavcan_bridge/uav_to_ros/consumed_charge_feedback.h>
 #include <sam_uavcan_bridge/uav_to_ros/consumed_charge_array.h>
 #include <sam_uavcan_bridge/uav_to_ros/ctd_feedback.h>
@@ -95,6 +96,7 @@ int main(int argc, char** argv)
     // uav_to_ros::ConversionServer<uavcan::equipment::esc::Status, uavcan_ros_msgs::ESCStatus> esc_status_server1(uav_node, pn, "esc_status1", 1);
     uav_to_ros::ConversionServer<uavcan::equipment::power::CircuitStatus, sam_msgs::CircuitStatusStamped> circuit_status_server(uav_node, pn, "circuit_status");
     uav_to_ros::ConversionServer<smarc_uavcan_messages::BatteryStateBasic, sensor_msgs::BatteryState> battery_server2(uav_node, pn, "battery_state_basic");
+    uav_to_ros::ConversionServer<uavcan::equipment::power::BatteryInfo, sensor_msgs::BatteryState> battery_server3(uav_node, pn, "battery_state_basic");
     uav_to_ros::ConversionServer<smarc_uavcan_messages::ConsumedChargeArray, sam_msgs::ConsumedChargeArray> consumed_charge_server2(uav_node, pn, "consumed_charge_array");
     uav_to_ros::ConversionServer<smarc_uavcan_messages::CTDFeedback, smarc_msgs::CTD> ctd_feedback_server(uav_node, pn, "ctd_feedback");
     uav_to_ros::ConversionServer<smarc_uavcan_messages::ThrusterFeedbackID, smarc_msgs::ThrusterFeedback> thruster1_feedback_server(uav_node, pn, "thruster1_feedback", 1);
