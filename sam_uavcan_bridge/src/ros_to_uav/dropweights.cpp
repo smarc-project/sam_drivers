e5b6b3932cbc60b705e86971a886f28a55e19b2e
Kurreman <korayamku@gmail.com>
2023-05-29 17:30:03 +0200

    :construction: bool message type

--- sam_uavcan_bridge/src/ros_to_uav/dropweights.cpp
+++ sam_uavcan_bridge/src/ros_to_uav/dropweights.cpp
@@ -9,13 +9,13 @@ bool convert(const std_msgs::Bool& ros_msg, uavcan::equipment::actuator::ArrayCo
     // const char* c = sub.c_str();
     // uav_msg.reason_text = c;
 
-    const std::string sub = ros_msg.data;//.substr(0, 7);
+    const std::bool boolean = ros_msg.data;//.substr(0, 7);
     // const char* c = sub.c_str();
 
     uav_msg.commands.resize(1);
     
     uav_msg.commands[0].actuator_id = uid;
-    uav_msg.commands[0].command_value = sub;//c;
+    uav_msg.commands[0].command_value = boolean;//c;
     uav_msg.commands[0].command_type = uavcan::equipment::actuator::Command::COMMAND_TYPE_POSITION;
     return true;
 }
